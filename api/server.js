c81e9eb81880148c2e492e2feffccc81b6aadb8b
Jonathon Green <jonathongre@gmail.com>
2019-09-04 11:59:20 -0500

    server running

---
+++ api/server.js
@@ -0,0 +1,20 @@
+const express = require('express')
+
+const server = express()
+
+server.get('/', (req, res) => {
+    res.status(200).json({ api: 'up' })
+})
+
+//manage products (8)
+//manage clients (8)
+//manage orders
+//manage suppliers
+
+server.get('/products', (req, res) => {})
+
+server.get('/clients', (req, res) => {})
+
+server.get('/suppliers', (req, res) => {})
+
+module.exports = server
---
+++ notes.md
@@ -0,0 +1,18 @@
+# Notes
+
+
+
+# Steps
+
+-create repo
+-clone repo
+-cd into folder
+-npm init -y
+-npx gitignore node
+-git add .
+-git commit -m "message"
+-git push -u origin master
+
+React applications are broken up into components.
+
+Express APIs are broken up into Express Routers.
